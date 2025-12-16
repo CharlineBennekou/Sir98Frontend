3957fe967011b99ca63103e51f7bc8838f1cc934
MathiasNoergaard <man010@edu.zealand.dk>
2025-12-16 13:51:22 +0100

    Fixed some more stuff in activateCode

--- src/components/users/CodeActivated.tsx
+++ src/components/users/CodeActivated.tsx
@@ -1,43 +1,32 @@
 import * as React from 'react';
 
-
-
 export class CodeActivationComp extends React.Component<{}, { progress: string}> {
-
     constructor(props: {}) {
         super(props);
         this.state = {
             progress: "load"
-        }
-        const parameters = new URLSearchParams(location.search)
-        const code: string | null = parameters.get("code");
-        console.log(code)
-        if(!code) {
-            this.state.progress
-        }
-        fetch(`https://sir98backendv3-hbbdgpawc0a8a3fp.canadacentral-01.azurewebsites.net/api/User/Activate/code=${code}`, {
-            method: 'GET',
-        }).then((response) => {
-            if(response.ok) {
-                this.setState({ progress: "ok" })
-            } else {
-                this.setState({ progress: "fail" })
-            }
-            console.log(response)
-        });
+        };   
     }
 
+    //to test this page disable strict mode
+    //https://stackoverflow.com/questions/72406486/react-fetch-api-being-called-2-times-on-page-load
     public render(): React.ReactNode {
-        
-        if(this.state.progress === "load" ) {
-            return (<h1>Aktiverer konto</h1>);
+        const parameters = new URLSearchParams(location.search)
+        const code: string | null = parameters.get("code");
+        if(this.state.progress === "load") {
+            fetch(`https://sir98backendv3-hbbdgpawc0a8a3fp.canadacentral-01.azurewebsites.net/api/User/Activate/code=${code}`, {
+                method: 'GET',
+            }).then((response) => {
+                this.setState({ progress: (response.status == 200) ? "ok" : "fail" })
+            });
+        }
+
+        if(this.state.progress === "fail" ) {
+           return (<h1>Ugyldig aktiveringskode.</h1>);
         }
         if(this.state.progress === "ok") {
             return (<h1>Konto er nu aktiveret</h1>);
         }
-        return (
-            <h1>Ugyldig aktiveringskode.</h1>
-        );
+         return (<h1>Aktiverer konto</h1>);
     }
 }
-
