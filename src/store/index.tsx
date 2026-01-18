5bd97b7727898653740893250139e435cd034fcd
MathiasNoergaard <man010@edu.zealand.dk>
2026-01-18 13:35:09 +0100

    Removed unused files

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -12,32 +12,3 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-
-
-// import { configureStore } from '@reduxjs/toolkit';
-// import { activitiesApi } from './apis/activityAPI';
-// import { instructorsApi } from './apis/instructorAPI';
-// import { api } from './apis/api';
-// //import { activityOccurrencesApi } from './apis/activityOccurrenceAPI';
-// //import { activitySubscriptionApi } from './apis/activitySubscriptionAPI';
-
-// export const store = configureStore({
-//   reducer: {
-//     [activitiesApi.reducerPath]: activitiesApi.reducer,
-//     [instructorsApi.reducerPath]: instructorsApi.reducer,
-//     [api.reducerPath]: api.reducer,
-//     //[activityOccurrencesApi.reducerPath]: activityOccurrencesApi.reducer,
-//     //[activitySubscriptionApi.reducerPath]: activitySubscriptionApi.reducer,
-//     // other reducers here
-//   },
-//   middleware: (getDefaultMiddleware) =>
-//     getDefaultMiddleware()
-//   .concat(activitiesApi.middleware, 
-//     instructorsApi.middleware, 
-//     api.middleware,
-//     //activityOccurrencesApi.middleware, 
-//     //activitySubscriptionApi.middleware,
-// )});
-
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
